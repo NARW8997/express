d8f6a5f17e21c5904e12adcb5c2745ee0d428b20
WangZeqing <70483027+NARW8997@users.noreply.github.com>
2023-05-09 23:28:27 -0400

    project initialize done

---
+++ reggie/src/main/java/com/heima/reggie/ReggieApplication.java
@@ -0,0 +1,17 @@
+package com.heima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zeqing wang
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("Start the project...");
+    }
+}
---
+++ reggie/src/main/java/com/heima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.heima.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("starting add resources path...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
